aefd49c394bb7da3c1307f744f70e7ad73c911fc
github_DASorescu <srsdln97@gmail.com>
2022-09-09 15:58:03 +0200

    constructor-and-method-setted

--- index.php
+++ index.php
@@ -7,7 +7,12 @@ class Movie{
     public $originalLanguage;
     public $cast;
     
-    function __construct($_id,$_title,$_trama,$_year,$_originalLanguage,$_cast)
+    function __construct($_id,$_title,$_trama,$_year,$_originalLanguage,$_cast);
+
+    public function setYear($year){
+        if(!is_numeric($year) || $year = 0) return;
+        $this-> $year = $year;
+    }
 }
 
 

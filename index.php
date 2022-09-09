212b89ac5fa52f3ec8b6fac4f472afc2ead3ef57
github_DASorescu <srsdln97@gmail.com>
2022-09-09 15:54:10 +0200

    class-initialized

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+class Movie{
+    public $id
+    public $title;
+    public $trama;
+    public $year;
+    public $originalLanguage;
+    public $cast;
+    
+    function __construct($_id,$_title,$_trama,$_year,$_originalLanguage,$_cast)
+}
+
+
+
+?>
